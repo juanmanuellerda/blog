b9898c6a00619ae2080856bd5e06adb34baa727e
Juan Manuel <juanmanuellerda@gmail.com>
2024-10-08 18:06:00 -0300

    pagina super basica funcionando

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,17 +5,77 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Hola Laravel</title>
+    <script src="https://cdn.tailwindcss.com"> </script>
 </head>
 <body>
     
-    <x-nav-link>
-        
-    </x-nav-link>
+<!-- <x-nav-link>
+   
+</x-nav-link>
     
+    
+    {{-- $slot --}}-->
 
-    {{-- <?php echo $slot ?> --}}
+    <!--
+  This example requires updating your template:
 
-    {{ $slot }}
+  ```
+  <html class="h-full bg-gray-100">
+  <body class="h-full">
+  ```
+-->
+<div class="min-h-full">
+  <nav class="bg-gray-800">
+    <div class="mx-left max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="flex h-16 items-center justify-between">
+        <div class="flex items-center">
+          <div class="flex-shrink-0">
+            <img class="h-8 w-8" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+          </div>
+          <div class="hidden md:block">
+            <div class="ml-10 flex items-baseline space-x-4">
+              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                <a href="/" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">home</a>
+                <a href="/pagina1" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">pagnina 1</a>
+                <a href="/pagina2" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Pagina 2</a>
+            </div>
+          </div>
+        </div>
+       
+        
+      </div>
+    </div>
+
+    <!-- Mobile menu, show/hide based on menu state. -->
+    <div class="md:hidden" id="mobile-menu">
+      <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+        <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+        <a href="/" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">home</a>
+        <a href="/pagina1" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">pagnina 1</a>
+        <a href="/pagina2" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Pagina 2</a>
+      </div>
+      <div class="border-t border-gray-700 pb-3 pt-4">
+        <div class="flex items-center px-5">
+      
+      </div>
+    </div>
+  </nav>
+
+  <header class="bg-white shadow">
+    <div class="mx-left max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900" >
+        
+      </h1>
+    </div>
+  </header>
+  
+  <main>
+    <div class="mx-left max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        {{$slot}}
+    </div>
+  </main>
+</div>
+
 
 
 </body>
