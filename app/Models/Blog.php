96909265e866e926852843878afd47e00107f7aa
Juan Manuel <juanmanuellerda@gmail.com>
2024-10-09 10:05:51 -0300

    cree nuevos elementos en tabla "Users" y "Blogs" usando tinker

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
-    /** @use HasFactory<\Database\Factories\BlogFactory> */
+    /* @use HasFactory<\Database\Factories\BlogFactory>
+    
+    protected $table = "users"; /*-----> con esta linea quito la convención de usar la tabla que se llama igual que 
+                                      el modeo pero en plural (tabla "blogs") ahora la tabla que se manejara
+                                      será la tabla "users" 
+    */
     use HasFactory;
 }
